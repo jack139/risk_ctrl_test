cbaca8b395b0d4c63fdbc5c2af66d3a7add64fdd
gt <jack139@gmail.com>
2022-06-08 10:09:43 +0800

    initial commit

---
+++ test.py
@@ -0,0 +1,181 @@
+import os
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore") ## 忽略版本警告
+from sklearn.model_selection import train_test_split
+import time,datetime
+import variable_bin_methods as varbin_meth ## 自定义函数，已上传至GitHub同文件夹
+import variable_encode as var_encode ## 自定义函数，已上传至GitHub同文件夹
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_curve, auc,confusion_matrix,recall_score,precision_score,accuracy_score
+from sklearn.linear_model import LogisticRegression
+from feature_selector import FeatureSelector ## 自定义函数，已上传至GitHub同文件夹
+from imblearn.over_sampling import SMOTE
+import missingno as msno
+import matplotlib
+matplotlib.use(backend='Qt5Agg')
+import matplotlib.pyplot as plt
+matplotlib.rcParams['font.sans-serif']=['SimHei']   
+matplotlib.rcParams['axes.unicode_minus']=False  
+#%matplotlib inline
+
+
+## 删除缺失值多的样本
+def del_na(df,colname_1,rate):
+    ## df: dataframe
+    ## colname_1: 列名list
+    ## rate:缺失值比例，大于rate变量删除
+    na_cols = df[colname_1].isna().sum().sort_values(ascending=False)/float(df.shape[0])
+    na_del = na_cols[na_cols >= rate]
+    df = df.drop(na_del.index, axis=1)
+    return df,na_del
+
+## 目标变量映射字典
+def target_mapping(lst):
+    ## Late (31-120 days)、Default、Charged Off映射为1，坏样本
+    ## Late (16-30 days)、In Grace Period映射为2,不确定样本
+    ## Current、Fully Paid映射为0，好样本
+    mapping = {}
+    for elem in lst:
+        if elem in ["Charged Off",'Late (16-30 days)', "Late (31-120 days)" ]:
+            mapping[elem] = 1
+        elif elem in ['In Grace Period']:
+            mapping[elem] = 2
+        elif elem in ['Current','Fully Paid']:
+            mapping[elem] = 0
+        else:
+            mapping[elem] = 3
+    return mapping  
+
+## 删除常量
+def constant_del(df, cols):
+    dele_list = []
+    for col in cols:
+        # remove repeat value counts
+        uniq_vals = list(df[col].unique())
+        if pd.isnull(uniq_vals).any():
+            if len( uniq_vals ) == 2:
+                dele_list.append(col)
+                print (" {} 变量只有一种取值,该变量被删除".format(col))
+        elif len(df[col].unique()) == 1:
+            dele_list.append(col)  
+            print (" {} 变量只有一种取值,该变量被删除".format(col))
+    df = df.drop(dele_list, axis=1)
+    return df,dele_list
+
+##删除长尾数据
+def tail_del(df,cols,rate):
+    dele_list = []
+    len_1 = df.shape[0]
+    for col in cols:
+        if len(df[col].unique()) < 5:
+            if df[col].value_counts().max()/len_1 >= rate:
+                dele_list.append(col)  
+                print (" {} 变量分布不均衡,该变量被删除".format(col))
+    df = df.drop(dele_list, axis=1)
+    return df,dele_list
+
+## 时间格式转化      
+def trans_format(time_string, from_format, to_format='%Y.%m.%d'):
+    ## from_format:原字符串的时间格式
+    ## param to_format:转化后的时间格式
+    if pd.isnull(time_string):
+        return np.nan
+    else:
+        time_struct = time.strptime(time_string,from_format)
+        times = time.strftime(to_format, time_struct)
+        times = datetime.datetime.strptime(times,'%Y-%m')
+        return times 
+
+## 离散变量与连续变量区分   
+def category_continue_separation(df,feature_names):
+    categorical_var = []
+    numerical_var = []
+    if 'target' in feature_names:
+        feature_names.remove('target')
+    ## 先判断类型，如果是int或float就直接作为连续变量
+    numerical_var = list(df[feature_names].select_dtypes(include=['int','float','int32','float32','int64','float64']).columns.values)
+    categorical_var = [x for x in feature_names if x not in numerical_var]
+    return categorical_var,numerical_var
+
+
+## 变量选择：iv筛选
+def iv_selection_func(bin_data, data_params, iv_low=0.02, iv_up=5, label='target'):
+    # 简单看一下IV，太小的不要
+    selected_features = []
+    for k, v in data_params.items():
+        if iv_low <= v < iv_up and k in bin_data.columns:
+            selected_features.append(k+'_woe')
+        else:
+            print('{0} 变量的IV值为 {1}，小于阈值删除'.format(k, v))
+    selected_features.append(label)
+    return bin_data[selected_features]
+
+def score_params_cal(base_point, odds, PDO):
+    ##给定预期分数，与翻倍分数，确定参数A,B
+    B = PDO/np.log(2)  
+    A = base_point + B*np.log(odds)
+    return A, B 
+
+def myfunc(x):
+    return str(x[0])+'_'+str(x[1])
+
+
+
+## 生成评分卡
+def create_score(dict_woe_map,dict_params,dict_cont_bin,dict_disc_bin):
+    ##假设Odds在1:60时对应的参考分值为600分，分值调整刻度PDO为20，则计算得到分值转化的参数B = 28.85，A= 481.86。
+    params_A,params_B = score_params_cal(base_point=600, odds=1/60, PDO=20)
+    # 计算基础分
+    base_points = round(params_A - params_B * dict_params['intercept'])
+    df_score = pd.DataFrame()
+    dict_bin_score = {}
+    for k in dict_params.keys():
+#        k='duration_BIN'
+#        k = 'foreign_worker_BIN'
+        if k !='intercept':
+            df_temp =  pd.DataFrame([dict_woe_map[k.split(sep='_woe')[0]]]).T
+            df_temp.reset_index(inplace=True)
+            df_temp.columns = ['bin','woe_val']
+            ## 计算分值
+            df_temp['score'] = round(-params_B*df_temp.woe_val*dict_params[k])
+            dict_bin_score[k.split(sep='_BIN')[0]] = dict(zip(df_temp['bin'],df_temp['score']))
+            ## 连续变量的计算
+            if k.split(sep='_BIN')[0] in dict_cont_bin.keys():
+                df_1 = dict_cont_bin[k.split(sep='_BIN')[0]]
+                df_1['var_name'] = df_1[['bin_low', 'bin_up']].apply(myfunc,axis=1)
+                df_1 = df_1[['total', 'var_name']]
+                df_temp = pd.merge(df_temp , df_1,on='bin')
+                df_temp['var_name_raw'] = k.split(sep='_BIN')[0]
+                df_score = pd.concat([df_score,df_temp],axis=0)
+            ## 离散变量的计算
+            elif k.split(sep='_BIN')[0] in dict_disc_bin.keys():
+                df_temp = pd.merge(df_temp , dict_disc_bin[k.split(sep='_BIN')[0]],on='bin')
+                df_temp['var_name_raw'] = k.split(sep='_BIN')[0]
+                df_score = pd.concat([df_score,df_temp],axis=0)
+
+    df_score['score_base'] =  base_points 
+    return df_score,dict_bin_score,params_A,params_B,base_points
+
+
+## 计算样本分数
+def cal_score(df_1,dict_bin_score,dict_cont_bin,dict_disc_bin,base_points):
+    ## 先对原始数据分箱映射，然后，用分数字典dict_bin_score映射分数，基础分加每项的分数就是最终得分
+    df_1.reset_index(drop=True,inplace = True)
+    df_all_score = pd.DataFrame()
+    ## 连续变量
+    for i in dict_cont_bin.keys():
+        if i in dict_bin_score.keys():
+            df_all_score = pd.concat([ df_all_score , varbin_meth.cont_var_bin_map(df_1[i], dict_cont_bin[i]).map(dict_bin_score[i]) ], axis = 1)
+    ## 离散变量
+    for i in dict_disc_bin.keys():
+        if i in dict_bin_score.keys():
+            df_all_score = pd.concat([ df_all_score ,varbin_meth.disc_var_bin_map(df_1[i], dict_disc_bin[i]).map(dict_bin_score[i]) ], axis = 1)
+    
+    df_all_score.columns = [x.split(sep='_BIN')[0] for x in list(df_all_score.columns)]
+    df_all_score['base_score'] = base_points    
+    df_all_score['score'] = df_all_score.apply(sum,axis=1)
+    df_all_score['target'] = df_1.target
+    return df_all_score
+
